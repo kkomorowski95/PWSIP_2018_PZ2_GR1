88025da97b1a680d496c09ec7eda38af9f561ac0
slawekzadrozny995 <slawekzadrozny995@gmail.com>
2018-04-03 15:25:31 +0200

    Avatary i Deck
    Skrypty jeszcze nie skończone

---
+++ GK/Assets/Code/c#/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+using UnityEngine.UI;
+using System.Text;
+using System.IO;
+public class Deck : MonoBehaviour {
+    public GameObject login;
+    public GameObject avatar;
+    public GameObject Karta;
+    public GameObject Info_K;
+    public GameObject Lista_P;
+    public GameObject Lista_Wybran;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
