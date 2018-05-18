035f588a2ca0d279d314167920653880ba3fdddb
kkomorowski95 <karolkomorowski95@gmail.com>
2018-05-18 20:22:23 +0200

    Poprawiono instrukcję obsługi dodając opis głównego menu, zasady gry oraz zmieniono wymagania minimalne na bardziej wiarygodne ( ͡º ͜ʖ͡º)

--- Strona/connect.php
+++ Strona/connect.php
@@ -1,8 +1,8 @@
 <?php
 
 	$host = "localhost";
-	$db_user = "root";
-	$db_password = "";
-	$db_name = "projekt";
+	$db_user = "DbUsr";
+	$db_password = "yuFG4TG71Zt50yqL";
+	$db_name = "Karcianka";
 
 ?>
